f0d5d6327d949e3736ee54f6dbc56d143a674734
Adam Lasut <adam.lasut@citadelo.com>
2024-03-17 09:33:16 +0100

    router fastfix

--- src/api/model/routes/model.js
+++ src/api/model/routes/model.js
@@ -6,16 +6,4 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = {
-  routes: [
-    createCoreRouter('api::model.model').routes,
-    {
-      method: 'POST',
-      path: '/model/query', // Custom path for your route
-      handler: 'query.forwardToMindsDB', // Handler in your controller
-      config: {
-        middlewares: ['api::model.mindsdb'], // Specify your middleware here
-      },
-    },
-  ],
-};
+module.exports =  createCoreRouter('api::model.model');
