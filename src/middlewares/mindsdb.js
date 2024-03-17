df262bc49f247c4d7d2183c107bf53029f1e19d7
mecaroman <144442498+mecaroman@users.noreply.github.com>
2024-03-17 13:38:26 +0100

    working mindsdb middleware mindsdb.js

--- src/middlewares/mindsdb.js
+++ src/middlewares/mindsdb.js
@@ -1,21 +1,24 @@
 const axios = require('axios');
 
-module.exports = (config, {strapi}) => {
+module.exports =  (config, {strapi}) => {
   return async (ctx, next) => {
     if (ctx.url.match(/^\/api\/model\/query/)) {
       try {
-        ctx.state.mindsdbResponse = await axios.post('https://lades.sk/api/sql/query', ctx.request.body, {
+        var response = await axios.post('https://lades.sk/api/sql/query', ctx.request.body, {
           headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${process.env.MINDSDB_BEARER_TOKEN}` // Use the environment variable
           },
         });
+
+        ctx.body = response.data;
+        
       } catch (error) {
         console.error('Error querying MindsDB:', error);
         ctx.send({ message: 'Failed to query MindsDB', error }, 500);
       }
     } else {
-      await next();
+      return next();
     }
   };
 };
